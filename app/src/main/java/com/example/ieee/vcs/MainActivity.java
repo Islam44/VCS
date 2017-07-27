7ffd9818001d92710f7986467be5daaa7dc8b3e8
IEEE <islamemam95@yahoo.com>
2017-07-27 18:55:17 +0200

    FB is not good

--- app/src/main/java/com/example/ieee/vcs/MainActivity.java
+++ app/src/main/java/com/example/ieee/vcs/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String user="ie";
         //facebook Is great
+        // acually is not good
     }
 }
